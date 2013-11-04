742fb618c7775ed0422ed8f793b6c21187660963
jackeyzhang <zhangchenhao@139.com>
2013-11-04 14:55:10 +0800

    commit code

--- src/com/sickle/medu/ms/client/form/RegisterDform.java
+++ src/com/sickle/medu/ms/client/form/RegisterDform.java
@@ -8,6 +8,8 @@ import com.sickle.pojo.edu.Member;
 import com.smartgwt.client.widgets.form.DynamicForm;
 import com.smartgwt.client.widgets.form.fields.FormItem;
 import com.smartgwt.client.widgets.form.fields.PasswordItem;
+import com.smartgwt.client.widgets.form.fields.RadioGroupItem;
+import com.smartgwt.client.widgets.form.fields.TextAreaItem;
 import com.smartgwt.client.widgets.form.fields.TextItem;
 
 
@@ -24,6 +26,11 @@ public class RegisterDform extends DynamicForm
 	private TextItem emailItem;
 	private PasswordItem password;
 	private PasswordItem confirmpassword;
+	private TextItem telephone;
+	private RadioGroupItem sex;
+	private TextAreaItem resume;
+	private TextItem orgname;
+	private TextItem title;
 	
 	public RegisterDform()
 	{
@@ -53,7 +60,32 @@ public class RegisterDform extends DynamicForm
 		confirmpassword.setRequired(true);
 		confirmpassword.setType("password");
 		
-		setFields(new FormItem[] {username, emailItem, password,confirmpassword});
+		telephone = new TextItem("contact");
+		telephone.setTitleStyle( "h1" );
+		telephone.setTitle("电话");
+		telephone.setRequired(true);
+		
+		sex = new RadioGroupItem( "sex" );
+		sex.setTitleStyle( "h1" );
+		sex.setTitle("性别");
+		sex.setRequired(true);
+		
+		resume = new TextAreaItem("resume");
+		resume.setTitleStyle( "h1" );
+		resume.setTitle("简介");
+		resume.setRequired(true);
+		
+		orgname = new TextItem("orgname");
+		orgname.setTitleStyle( "h1" );
+		orgname.setTitle("工作单位");
+		orgname.setRequired(true);
+		
+		title = new TextItem("title");
+		title.setTitleStyle( "h1" );
+		title.setTitle("职位");
+		title.setRequired(true);
+		
+		setFields(new FormItem[] {username, emailItem, password,confirmpassword,telephone,sex,resume,orgname,title});
 		setDataSource( MemberDataSource.getInstance( ).getDataSource( Member.class ) );
 	}
 
