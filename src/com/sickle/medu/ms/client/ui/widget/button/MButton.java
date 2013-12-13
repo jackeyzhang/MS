d36768fcf3f6753f7c43f7429530d72255d4818c
jackeyzhang <zhangchenhao@139.com>
2013-12-13 10:48:54 +0800

    accomplish MButton style

--- src/com/sickle/medu/ms/client/ui/widget/button/MButton.java
+++ src/com/sickle/medu/ms/client/ui/widget/button/MButton.java
@@ -1,6 +1,7 @@
 /**
  * 
  */
+
 package com.sickle.medu.ms.client.ui.widget.button;
 
 import com.sickle.medu.ms.client.ui.panel.MouseChangePanel;
@@ -8,48 +9,44 @@ import com.sickle.medu.ms.client.ui.widget.label.LabelWithWhite;
 import com.smartgwt.client.widgets.events.ClickEvent;
 import com.smartgwt.client.widgets.events.ClickHandler;
 
-
-
-
 /**
  * MEDU Button
  * 
- * default width: 50px height:25px
- * default style: 黄底白字 over时变褐色
+ * default width: 50px height:25px default style: 黄底白字 over时变褐色
  * 
  * @author chenhao
- *
+ * 
  */
 public abstract class MButton extends MouseChangePanel
 {
 
 	private LabelWithWhite label = null;
-	
-	
-	public MButton( String title,String width)
+
+	public MButton( String title, String width )
 	{
+		super( true, "mbutton", "mbuttonin" );
 		this.setWidth( width );
 		this.setHeight( 25 );
-		label = new LabelWithWhite(title,true,"whitelittlelabel","whitelittlelabel");
+		label = new LabelWithWhite( title, true, "whitelittlelabel",
+				"whitelittlelabel" );
 		label.setWidth( width );
 		this.addMember( label );
-		
+
 		label.addClickHandler( new ClickHandler( ) {
-			
+
 			@Override
 			public void onClick( ClickEvent event )
 			{
-				handleClick();				
+				handleClick( );
 			}
 		} );
 	}
-	
-	public MButton( String title)
+
+	public MButton( String title )
 	{
-		this(title,"100px");
+		this( title, "100px" );
 	}
-	
-	public abstract void handleClick();
-	
+
+	public abstract void handleClick( );
 
 }
